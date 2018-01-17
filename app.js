b45c953bc18f6207536ebe9dc436721a0adfc887
Lyudmil Latinov <llatinov@gmail.com>
2018-01-17 09:34:07 +0200

    Add Express with Hello world response.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hello world!');
+const express = require('express');
+const app = new express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server up!');
+});
