f39efd32524a82454abc1aa9d1ccdea0d5e6a44a
Lyudmil Latinov <llatinov@gmail.com>
2019-04-23 16:24:06 +0200

    Format code with new Prettier rules.

--- repo/personRepository.js
+++ repo/personRepository.js
@@ -3,38 +3,37 @@
 const Person = require('../json/person');
 
 class PersonRepository {
-    constructor() {
-        this.persons = new Map([
-            [1, new Person(1, 'FN1', 'LN1', 'email1@email.na')],
-            [2, new Person(2, 'FN2', 'LN2', 'email2@email.na')],
-            [3, new Person(3, 'FN3', 'LN3', 'email3@email.na')],
-            [4, new Person(4, 'FN4', 'LN4', 'email4@email.na')]
-        ]);
-    }
+  constructor() {
+    this.persons = new Map([
+      [1, new Person(1, 'FN1', 'LN1', 'email1@email.na')],
+      [2, new Person(2, 'FN2', 'LN2', 'email2@email.na')],
+      [3, new Person(3, 'FN3', 'LN3', 'email3@email.na')],
+      [4, new Person(4, 'FN4', 'LN4', 'email4@email.na')],
+    ]);
+  }
 
-    getById(id) {
-        return this.persons.get(id);
-    }
+  getById(id) {
+    return this.persons.get(id);
+  }
 
-    getAll() {
-        return Array.from(this.persons.values());
-    }
+  getAll() {
+    return Array.from(this.persons.values());
+  }
 
-    remove() {
-        const keys = Array.from(this.persons.keys());
-        this.persons.delete(keys[keys.length - 1]);
-    }
+  remove() {
+    const keys = Array.from(this.persons.keys());
+    this.persons.delete(keys[keys.length - 1]);
+  }
 
-    save(person) {
-        if (this.getById(person.id) !== undefined) {
-            this.persons[person.id] = person;
-            return 'Updated Person with id=' + person.id;
-        }
-        else {
-            this.persons.set(person.id, person);
-            return 'Added Person with id=' + person.id;
-        }
+  save(person) {
+    if (this.getById(person.id) !== undefined) {
+      this.persons[person.id] = person;
+      return 'Updated Person with id=' + person.id;
+    } else {
+      this.persons.set(person.id, person);
+      return 'Added Person with id=' + person.id;
     }
+  }
 }
 
 const personRepository = new PersonRepository();
--- routes/versionRoutes.js
+++ routes/versionRoutes.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const getVersionRoutes = (app, config) => {
-    app.get('/api/version', (req, res) => {
-        res.send(config.version);
-    });
+  app.get('/api/version', (req, res) => {
+    res.send(config.version);
+  });
 };
 
 module.exports = getVersionRoutes;
