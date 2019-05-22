f8e2c0a76c8d411312664f6627edaaf13291ae5d
Lyudmil Latinov <llatinov@gmail.com>
2019-05-22 15:45:18 +0200

    Add CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = new express();
 // register JSON parser middlewear
 app.use(bodyParser.json());
 
+require('./config/cors')(app);
 require('./routes/personRoutes')(app);
 require('./routes/versionRoutes')(app, config);
 
